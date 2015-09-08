bdbab91d2c3d5858f73b81ced867999e32e2db4b
Nathan Boley <npboley@gmail.com>
2015-09-08 07:56:39 -0700

    Added pickled human and mouse motifs to data.

--- scripts/fast_score_regions_from_db_motifs.py
+++ scripts/fast_score_regions_from_db_motifs.py
@@ -1,16 +1,65 @@
 import os, sys
+import numpy as np
 
-conn = psycopg2.connect("host=mitra dbname=cisbp user=nboley")
+import cPickle as pickle
+
+import psycopg2
+
+from pysam import FastaFile, TabixFile
+
+from collections import namedtuple
+PwmModel = namedtuple('PwmModel', [
+    'tf_id', 'motif_id', 'tf_name', 'tf_species', 'pwm']) 
+
+pickled_motifs_fname = os.path.join(
+    os.path.dirname(__file__), 
+    "../data/motifs/human_and_mouse_motifs.pickle.obj")
+
+def load_regions_in_bed(fp):
+    regions = []
+    for line in fp:
+        data = line.split()
+        contig = data[0]
+        start = int(data[1])
+        stop = int(data[2])
+        regions.append((contig, start, stop))
+    return regions
 
 def load_all_motifs():
+    try: 
+        with open(pickled_motifs_fname, "r") as fp:
+            return pickle.load(fp)
+    except: pass
+
+    conn = psycopg2.connect("host=mitra dbname=cisbp user=nboley")
     cur = conn.cursor()    
-    query = "select tf_id, motif_id, tf_name, tf_species, pwm from related_motifs_mv NATURAL JOIN pwms where tf_species = 'Homo_sapiens' and rank = 1;"
+    query = """
+    SELECT tf_id, motif_id, tf_name, tf_species, pwm 
+      FROM related_motifs_mv NATURAL JOIN pwms 
+     WHERE tf_species in ('Mus_musculus', 'Homo_sapiens') 
+       AND rank = 1;"""
     cur.execute(query)
-    for res in cur.fetchall():
-        print res
-        break
+    motifs = []
+    for data in cur.fetchall():
+        data = list(data)
+        data[-1] = np.log2(np.array(data[-1]) + 1e-4)
+        motifs.append( PwmModel(*data) )
+
+    with open(pickled_motifs_fname, "w") as fp:
+        return pickle.dump(motifs, fp)
+
+    return motifs
 
 def main():
-    motifs = load_all_motifs()
+    genome_fname = sys.argv[1]
+    regions_fname = sys.argv[2]
 
+    genome = FastaFile(genome_fname)
+    print "Loaded genome"
+    with open(regions_fname) as fp:
+        regions = load_regions_in_bed(fp)
+    print "Loaded regions"
+    motifs = load_all_motifs()
+    print "Loaded motifs"
+    
 main()
