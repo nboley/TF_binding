476f1b70d72a6f3c7ced94588c2cb96c4fb6a4d6
jisraeli <jisraeli@stanford.edu>
2015-09-30 16:48:57 -0700

    added data subsetting

--- rSeqDNN/TrainModel.py
+++ rSeqDNN/TrainModel.py
@@ -2,17 +2,17 @@ import os
 import argparse
 import re
 import gzip
+from collections import namedtuple
+from itertools import izip
 
 import numpy as np
 from scipy import misc
-from collections import namedtuple
+
 from pysam import FastaFile
 
 from pyTFbindtools.peaks import load_summit_centered_peaks, load_narrow_peaks
 from pyTFbindtools.sequence import code_seq
-
-class PeaksAndLabels(list):
-    pass
+from pyTFbindtools.cross_validation import iter_train_validation_splits
 
 PeakAndLabel = namedtuple('PeakAndLabel', ['peak', 'sample', 'label'])
 
@@ -23,7 +23,6 @@ def getFileHandle(filename,mode="r"):
         return gzip.open(filename,mode)
     else:
         return open(filename,mode)
-
 
 def encode_peaks_sequence_into_binary_array(peaks, fasta):
         # find the peak width
@@ -37,12 +36,45 @@ def encode_peaks_sequence_into_binary_array(peaks, fasta):
             data[i] = coded_seq[0:4,:]
         return data
 
-class PeakRegionData():
-    def __init__(self, peaks_and_labels, fasta):
-        assert isinstance(peaks_and_labels, PeaksAndLabels)
-        self.coded_peaks_seqs = encode_peaks_sequence_into_binary_array(
-            [pk for pk, sample, label in peaks_and_labels], fasta)
-        self.peaks_and_labels = peaks_and_labels
+class PeaksAndLabels():
+    def __iter__(self):
+        return (
+            PeakAndLabel(pk, sample, label) 
+            for pk, sample, label 
+            in izip(self.peaks, self.samples, self.labels)
+        )
+    
+    def __init__(self, peaks_and_labels):
+        # split the peaks and labels into separate columns. Also
+        # keep track of the distinct samples and contigs
+        self.peaks = []
+        self.samples = []
+        self.labels = []
+        self.sample_ids = set()
+        self.contigs = set()
+        for pk, sample, label in peaks_and_labels:
+            self.peaks.append(pk)
+            self.samples.append(sample)
+            self.labels.append(label)
+            self.sample_ids.add(sample)
+            self.contigs.add(pk.contig)
+        
+        # turn the list of labels into a numpy array
+        self.labels = np.array(self.labels, dtype=int)
+    
+    # One hot encode the sequence in each peak
+    def build_coded_seqs(self, fasta):
+        return encode_peaks_sequence_into_binary_array(
+            self.peaks, fasta)
+    
+    def subset_data(self, sample_names, contigs):
+        '''return data covering sample+names and contigs
+        '''
+        return PeaksAndLabels(
+                pk_and_label for pk_and_label in self 
+                if pk_and_label.sample in sample_names
+                and pk_and_label.peak.contig in contigs
+            )
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -62,15 +94,16 @@ def parse_args():
 def main():
     args = parse_args()
     genome_fasta = args.genome_fasta
-    peaks_and_labels = PeaksAndLabels()
+    peaks_and_labels = []
     for pos_pk in load_summit_centered_peaks(
             load_narrow_peaks(args.pos_regions), args.half_peak_width):
         peaks_and_labels.append((pos_pk, 'sample', 1))
     for neg_pk in load_summit_centered_peaks(
             load_narrow_peaks(args.neg_regions), args.half_peak_width):
         peaks_and_labels.append((neg_pk, 'sample', 0))
-    peaks = PeakRegionData(peaks_and_labels, genome_fasta)
-    print peaks.coded_peaks_seqs
+    peaks = PeaksAndLabels(peaks_and_labels)
+    train = peaks.subset_data(['sample'], ['chr1'])
+    print train.build_coded_seqs(genome_fasta)
 
 if __name__ == '__main__':
     main()
