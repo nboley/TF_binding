89258fa3d39f9c8b0abd22f24b010a0a9babd477
jisraeli <jisraeli@stanford.edu>
2015-10-23 16:38:41 -0700

    initial commit: trains rSeqDNN for same celltype predictions for comparison with deepsea

---
+++ rSeqDNN/CompareModel.py
@@ -0,0 +1,109 @@
+import os
+
+from pysam import FastaFile
+
+from rSeqDNN import init_prediction_script_argument_parser
+
+from pyTFbindtools.peaks import (
+    load_labeled_peaks_from_beds, 
+    load_chromatin_accessible_peaks_and_chipseq_labels_from_DB
+)
+
+from pyTFbindtools.cross_validation import ClassificationResults
+
+from KerasModel import KerasModel, encode_peaks_sequence_into_binary_array
+
+def parse_args():
+    parser = init_prediction_script_argument_parser(
+        'main script for estimating rSeqDNN accuracy via cross validation')
+    parser.add_argument('--model-prefix', default='trainedmodel',
+        help='Trained models will be written to (model_prefix).foldnum.h5"')
+    parser.add_argument('--only-test-one-fold', 
+                        default=False, action='store_true',
+        help='Only evaluate on a single cross validation fold.')
+
+    args = parser.parse_args()
+    
+    assert args.annotation_id is not None or args.genome_fasta is not None, \
+        "Must set either --annotation-id or --genome-fasta"
+    if args.genome_fasta is None:
+        assert args.annotation_id is not None
+        from pyTFbindtools.DB import load_genome_metadata
+        genome_fasta = FastaFile(
+            load_genome_metadata(args.annotation_id).filename) 
+    else:
+        genome_fasta = args.genome_fasta
+    
+    if args.tf_id is not None:
+        assert args.pos_regions is None and args.neg_regions is None, \
+            "It doesnt make sense to set both --tf-id and either --pos-regions or --neg-regions"
+        assert args.annotation_id is not None, \
+            "--annotation-id must be set if --tf-id is set"
+        assert args.genome_fasta is None, \
+            "if --tf-id is set the genome fasta must be specified by the --annotation-id"
+
+        peaks_and_labels = load_chromatin_accessible_peaks_and_chipseq_labels_from_DB(
+            args.tf_id,
+            args.annotation_id,
+            args.half_peak_width, 
+            args.max_num_peaks_per_sample, 
+            include_ambiguous_peaks=True)
+    else:
+        assert args.pos_regions != None and args.neg_regions != None, \
+            "either --tf-id or both (--pos-regions and --neg-regions) must be set"
+        peaks_and_labels = load_labeled_peaks_from_beds(
+            args.pos_regions, args.neg_regions, args.half_peak_width)
+    
+    return ( peaks_and_labels, 
+             genome_fasta, 
+             args.model_prefix, 
+             args.only_test_one_fold,
+             args.skip_ambiguous_peaks)
+
+import cPickle as pickle
+
+def main():
+    ( peaks_and_labels, 
+      genome_fasta, 
+      model_ofname_prefix, 
+      only_test_one_fold,
+      skip_ambiguous_peaks
+    ) = parse_args()
+    validation_contigs = ['chr8', 'chr9']
+    model = KerasModel(peaks_and_labels)
+    results = ClassificationResults()
+    for fold_index, sample in enumerate(peaks_and_labels.sample_ids):
+        print 'sample: ', sample
+        print 'subsetting to deepsea test data...'
+        valid = peaks_and_labels.subset_data([sample],
+                                             validation_contigs)
+        train = peaks_and_labels.remove_data([],
+                                             validation_contigs) 
+        fit_model = model.train(
+            train, 
+            genome_fasta, 
+            '%s.%i.hd5' % (model_ofname_prefix, fold_index+1))
+
+        clean_res = fit_model.evaluate_peaks_and_labels(
+            valid, 
+            genome_fasta,
+            filter_ambiguous_labels=True)
+        print "CLEAN:", clean_res
+        
+        res = fit_model.evaluate_peaks_and_labels(
+            valid, 
+            genome_fasta,
+            filter_ambiguous_labels=False,
+            plot_fname=("ambig.fold%i.png" % fold_index))
+        print "FULL:", res
+        results.append(res)
+        
+        if only_test_one_fold: break
+    
+    print 'Printing cross validation results:'
+    for res in results:
+        print res
+    print results
+
+if __name__ == '__main__':
+    main()
