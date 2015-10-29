Fixed the energy calculation function so that it takes the reverse complement into account.

bin/fit_selex.py
@@ -15,7 +15,7 @@ import pyTFbindtools.selex
 from pyTFbindtools.selex.log_lhd import calc_log_lhd, calc_binding_site_energies
 
 from pyTFbindtools.selex import (
-    find_pwm, code_seqs, calc_occ,
+    find_pwm, code_seq, code_seqs, code_RC_seqs, calc_occ,
     estimate_dg_matrix_with_adadelta,
     find_pwm_from_starting_alignment,
     PartitionedAndCodedSeqs, base_map,
@@ -85,9 +85,9 @@ def load_text_file(fp, maxnum=1e9):
 def load_fastq(fp, maxnum=1e9):
     seqs = []
     for i, line in enumerate(fp):
+        if i/4 >= maxnum: break
         if i%4 == 1:
             seqs.append(line.strip().upper())
-            if i/4 > maxnum: break
     return seqs
 
 def load_sequences(fnames):
@@ -99,7 +99,7 @@ def load_sequences(fnames):
         opener = gzip.open if fname.endswith(".gz") else open  
         with opener(fname) as fp:
             loader = load_fastq if ".fastq" in fname else load_text_file
-            rnds_and_seqs[rnd] = loader(fp) # , 100
+            rnds_and_seqs[rnd] = loader(fp, 178) # , 100
     return rnds_and_seqs
 
 def write_output(motif, ddg_array, ref_energy, ofp=sys.stdout):
@@ -146,8 +146,6 @@ def parse_arguments():
                          help='Convergence tolerance for lhd change.')
     parser.add_argument( '--max-iter', type=float, default=1e5,
                          help='Maximum number of optimization iterations.')
-    parser.add_argument( '--momentum', type=float, default=0.1,
-                         help='Optimization tuning param (between 0 and 1).')
 
     parser.add_argument( '--random-seed', type=int,
                          help='Set the random number generator seed.')
@@ -169,8 +167,6 @@ def parse_arguments():
 
     pyTFbindtools.selex.CONVERGENCE_MAX_LHD_CHANGE = args.lhd_convergence_eps
     pyTFbindtools.selex.MAX_NUM_ITER = int(args.max_iter)
-    assert args.momentum < 1 and args.momentum >= 0
-    pyTFbindtools.selex.MOMENTUM = args.momentum
     
     if args.random_seed != None:
         np.random.seed(args.random_seed)
@@ -243,6 +239,27 @@ def find_best_shift(coded_seqs, ddg_array):
     if left_base_score < right_base_score:
         return 'LEFT'
     return 'RIGHT'
+
+def test_RC_equiv():
+    """Make sure that the RC function works correctly.
+    
+    """
+    seq = 'GCGAATACC'
+    coded_seq = code_seq(seq)
+    RC_map = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    RC_seq = "".join(RC_map[x] for x in seq[::-1])
+    coded_RC_seq = code_seq(RC_seq)
+    
+    print coded_seq
+    print coded_RC_seq
+    print calc_binding_site_energies(coded_seq[None,(1,2,3),:], ddg_array)
+    print calc_binding_site_energies(coded_RC_seq[None, (1,2,3),:], ddg_array)[:,::-1]
+    energy_diff, RC_ddg_array = ddg_array.reverse_complement()
+    print energy_diff + calc_binding_site_energies(coded_seq[None, (1,2,3),:], RC_ddg_array)
+    #print -energy_diff + calc_binding_site_energies(coded_seq[None,(1,2,3),:], RC_ddg_array)
+    return
+
+    pass
 
 def fit_model(rnds_and_seqs, ddg_array, ref_energy):
     pyTFbindtools.log("Coding sequences", 'VERBOSE')

pyTFbindtools/selex/__init__.py
@@ -1,21 +1,16 @@
 import os, sys
 import math
 
-from itertools import product, izip, chain
+from itertools import izip, chain
 
 from collections import defaultdict, namedtuple
 
 import numpy as np
 np.random.seed(0)
 
-import matplotlib.pyplot as plt
-
 import theano
 
-from scipy.optimize import (
-    minimize, minimize_scalar, brentq, approx_fprime )
-from scipy.signal import convolve2d
-from numpy.fft import rfft, irfft
+from scipy.optimize import brentq, approx_fprime
 
 import random
 
@@ -28,21 +23,16 @@ from log_lhd import calc_log_lhd
 
 PARTITION_FN_SAMPLE_SIZE = 10000
 
-CMP_LHD_NUMERATOR_CALCS = False
-RANDOM_POOL_SIZE = None
 CONVERGENCE_MAX_LHD_CHANGE = None
 MAX_NUM_ITER = 10000
 
-EXPECTED_MEAN_ENERGY = -3.0
 CONSTRAIN_MEAN_ENERGY = True
+EXPECTED_MEAN_ENERGY = -3.0
 
 CONSTRAIN_BASE_ENERGY_DIFF = True
 MAX_BASE_ENERGY_DIFF = 8.0
 
 USE_SHAPE = False
-
-# during optimization, how much to account for previous values
-MOMENTUM = None
 
 RC_map = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
 base_map_dict = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 0: 0, 1: 1, 2: 2, 3: 3}
@@ -176,8 +166,27 @@ def code_seqs(seqs, seq_len, n_seqs=None, ON_GPU=True):
         coded_seq = code_seq(seq)
         # ignore the A and N rows
         coded_seqs[i,:,:] = coded_seq[(1,2,3),:]
-
     return coded_seqs
+
+def code_RC_seqs(seqs, seq_len, n_seqs=None, ON_GPU=True):
+    """Load SELEX data and encode all the subsequences. 
+
+    """
+    if n_seqs == None: n_seqs = len(seqs)
+
+    # leave 3 rows for each sequence base, and 6 for the shape params
+    len_per_base = 3
+    if USE_SHAPE:
+        len_per_base += 6
+
+    coded_seqs = np.zeros((n_seqs, len_per_base, seq_len), dtype='float32')
+    for i, seq in enumerate(seqs):
+        # code seq, and then toss the A and N rows
+        coded_seq = code_seq(seq)
+        # ignore the A and N rows
+        coded_seqs[i,:,:] = np.fliplr(np.flipud(coded_seq[:4,:]))[(1,2,3),:]
+    return coded_seqs
+
 
     #if ON_GPU:
     #    return theano.shared(coded_seqs)

pyTFbindtools/selex/log_lhd.py
@@ -15,6 +15,19 @@ PARTITION_FN_SAMPLE_SIZE = 1000
 
 random_seqs = {}
 
+def test_calc_affinities():
+    """Place holder for some test code.
+
+    """
+    seq = 'GCGAATACC'
+    coded_seq = code_seq(seq)
+    RC_map = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    RC_seq = "".join(RC_map[x] for x in seq[::-1])
+    coded_RC_seq = code_seq(RC_seq)
+
+    print coded_seqs.shape
+    print calc_affinities(coded_seqs, ddg_array )
+    
 def log_lhd_factory():
     import theano
     import theano.tensor as TT
@@ -23,6 +36,22 @@ def log_lhd_factory():
     # ignore theano warnings
     import warnings
     warnings.simplefilter("ignore")
+    
+    ## Theano function to calculate affinities
+    seqs = TT.tensor3(name='seqs')
+    ddg = TT.matrix(name='ddg')
+    fwd_bs_affinities = theano_conv2d(seqs, ddg[::-1,::-1])[:,0,:]
+    rc_ddg = (
+        TT.concatenate((ddg[(1,0),:], TT.zeros_like(ddg[(0,),:])), axis=0) 
+        - ddg[2,:])[:,::-1]
+    rc_bs_affinities = (
+        theano_conv2d(seqs, rc_ddg[::-1,::-1]) + ddg[2,:].sum()
+    )[:,0,:]
+
+    bs_affinities = TT.stack(
+        (fwd_bs_affinities, rc_bs_affinities), axis=1).min(1)
+    seq_affinities = bs_affinities.min(1)
+    calc_affinities = theano.function([seqs, ddg], seq_affinities )
 
     sym_occ = TT.vector()
     sym_part_fn = TT.vector()
@@ -33,9 +62,7 @@ def log_lhd_factory():
 
     sym_seqs = TT.tensor3(name='seqs')
     sym_ddg = TT.matrix(name='ddg')
-    calc_energy_fn = theano.function(
-        [sym_seqs, sym_ddg], theano_conv2d(sym_seqs, sym_ddg).min(2)[:,0])
-
+    
     sym_e = TT.vector()
 
     calc_occ = theano.function([sym_chem_pot, sym_e], 
@@ -101,7 +128,7 @@ def log_lhd_factory():
             prot_conc):
         # now calculate the denominator (the normalizing factor for each round)
         # calculate the expected bin counts in each energy level for round 0
-        energies = ref_energy + calc_energy_fn(
+        energies = ref_energy + calc_affinities(
             random_seqs[seq_len], ddg_array)
         expected_cnts = (4**seq_len)/float(len(random_seqs[seq_len]))
         curr_occupancies = expected_cnts*np.ones(
@@ -143,7 +170,7 @@ def log_lhd_factory():
         # score all of the sequences
         rnds_and_seq_ddgs = {}
         for rnd, rnd_coded_seqs in coded_seqs.iteritems():
-            rnds_and_seq_ddgs[rnd] = calc_energy_fn(rnd_coded_seqs, ddg_array)
+            rnds_and_seq_ddgs[rnd] = calc_affinities(rnd_coded_seqs, ddg_array)
 
         # calcualte the denominators
         chem_affinities, denominators = calc_lhd_denominators_and_chem_pots(
@@ -180,7 +207,7 @@ def calc_binding_site_energies(coded_seqs, ddg_array):
     n_bind_sites = coded_seqs.shape[2]-ddg_array.shape[1]+1
     rv = np.zeros((n_seqs, n_bind_sites), dtype='float32')
     for i, coded_seq in enumerate(coded_seqs):
-        rv[i,:] = convolve(coded_seq, ddg_array, mode='valid')
+        rv[i,:] = convolve(coded_seq, np.fliplr(np.flipud(ddg_array)), mode='valid')
     return rv
 
 #calc_log_lhd = None