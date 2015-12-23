945b08990f92ed07c7a6ef577a8eb5f42910d7cf
jisraeli <jisraeli@stanford.edu>
2015-12-23 11:46:22 -0800

    added filter to pwm comparison utilities

--- rSeqDNN/ScoreModel.py
+++ rSeqDNN/ScoreModel.py
@@ -1,12 +1,17 @@
 import os
+import glob
 import sys
 import numpy as np
 from scipy.stats import rankdata
+from scipy.signal import correlate2d
 from sklearn.metrics import roc_auc_score
 from matplotlib import (
     pyplot as plt,
     image as mpimg )
- 
+from pyTFbindtools.peaks import getFileHandle
+
+ENCODE_MOTIFS_PATH =  os.path.join(os.getcwd(),'encode_motifs.txt.gz')
+
 def score_convolutions(model, X, batch_size):
     """
     Score convolutions using deepLifft on maxpooling layer.
@@ -99,6 +104,102 @@ def rank_convolutions(convolution_scores, y_true, y_pred, rank_metric='auROC'):
         return rankdata(specificity_scores)
     else:
         raise ValueError('Invalid rank option!')
+
+class PWM(object):
+    def __init__(self, weights, name=None, threshold=None):
+        self.weights = weights
+        self.name = name
+        self.threshold = threshold
+
+    @staticmethod
+    def from_homer_motif(motif_file):
+        with getFileHandle(motif_file) as fp:
+            header = fp.readline().strip().split('\t')
+            name = header[1]
+            threshold = float(header[2])
+            weights = np.loadtxt(fp)
+
+        return PWM(weights, name, threshold)
+
+    @staticmethod
+    def get_encode_pwms(motif_file):
+        pwms = []
+        with getFileHandle(motif_file) as fp:
+            line = fp.readline().strip()
+            while True:
+                if line == '':
+                    break
+
+                header = line
+                weights = []
+                while True:
+                    line = fp.readline()
+                    if line == '' or line[0] == '>':
+                        break
+                    weights.append(map(float, line.split()[1:]))
+                pwms.append(PWM(np.array(weights), header))
+
+        return pwms
+
+    @staticmethod
+    def from_cisbp_motif(motif_file):
+        name = os.path.basename(motif_file)
+        with getFileHandle(motif_file) as fp:
+            _ = fp.readline()
+            weights = np.loadtxt(fp)[:, 1:]
+        return PWM(weights, name)
+
+def max_min_cross_corr(pwm, conv_filter):
+    if conv_filter.shape[1] != 4:
+        conv_filter = conv_filter.T
+    assert conv_filter.shape[1] == 4
+    assert pwm.shape[1] == 4
+    corr = correlate2d(pwm, conv_filter, mode='same')
+    # we are only interested in the part where the 'letter' axes are aligned,
+    # and the correlation is over the position axis only, which is in the 2nd
+    # column
+    allowed_corr = corr[:, 1]
+    max_corr = np.max(allowed_corr)
+    min_corr = np.min(allowed_corr)
+    # max_pos (and min_pos) relates to the alignment between the pwm and the
+    # conv_filter as follows - more generally,
+    #
+    # Position floor(w / 2) - i maps to position 0 on the (padded) PWM
+    #
+    # where w = width of conv filter, and i is the position given by best_pos
+    # (or more generally, the index into allowed_corr). If a negative position
+    # is obtained, that means that position 0 on the conv_filter maps to a
+    # later position on the PWM.
+    max_pos = np.argmax(allowed_corr)
+    min_pos = np.argmin(allowed_corr)
+
+    return ((max_corr, max_pos), (min_corr, min_pos))
+
+def get_motif_matches(filters, pwms, topk=5):
+    filterhits = []
+    for conv_filter in filters:                
+        conv_filter = conv_filter.squeeze()
+        min_activation = conv_filter.min(axis=0).sum()
+        max_activation = conv_filter.max(axis=0).sum()
+        activation_range = max_activation - min_activation
+        def norm_cc(cc):
+            return (cc - min_activation) / activation_range
+        hits = []
+        for idx, pwm in enumerate(pwms):
+            (max_cc, max_pos), (min_cc, min_pos) = \
+                max_min_cross_corr(pwm.weights, conv_filter)
+            hits.append((norm_cc(max_cc), max_pos, pwm.name))
+            hits.append((norm_cc(min_cc), min_pos, pwm.name))
+        hits.sort(reverse=True)
+        filterhits.append(hits[:topk])
+
+    return filterhits
+
+def get_encode_pwm_hits(model):
+    conv_weights, _ = model.layers[0].get_weights()
+    encode_pwms = PWM.get_encode_pwms(ENCODE_MOTIFS_PATH)
+
+    return get_motif_matches(conv_weights, encode_pwms)
 
 def plot_convolutions(model, ofname, rank_dictionary=None):
     """
