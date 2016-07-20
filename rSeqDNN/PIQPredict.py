9adc2b419e1aec53756d99eb8bfc7c49543605af
Aakash Ravi <aakash_ravi@hotmail.com>
2016-07-20 14:36:51 -0700

    Unit testing bed file functionality

--- rSeqDNN/PIQPredict.py
+++ rSeqDNN/PIQPredict.py
@@ -1,12 +1,21 @@
 import csv
 import sys
-from pyTFbindtools.peaks import (
-    load_labeled_peaks_from_beds, 
-    getFileHandle, 
-    load_chromatin_accessible_peaks_and_chipseq_labels_from_DB,
-    PeaksAndLabelsThreadSafeIterator
-)
-from pybedtools import Interval, BedTool
+import subprocess
+# sys.path.append("..")
+# sys.path.append("../../pyDNABinding/")
+
+# import pyximport
+# pyximport.install()
+
+# from pyTFbindtools.peaks import (
+#     load_labeled_peaks_from_beds, 
+#     getFileHandle, 
+#     load_chromatin_accessible_peaks_and_chipseq_labels_from_DB,
+#     PeaksAndLabelsThreadSafeIterator
+# )
+# from pybedtools import Interval, BedTool
+
+
 
 piq_factor_dict = {}
 piq_factor_dict['MAX'] = 'MA0058.1 MAX'
@@ -93,16 +102,55 @@ def create_piq_bed_file(score_csv_file, output_peak_file):
 def main():
     input_csv_file = sys.argv[1]
     output_bed_file = sys.argv[2]
+    # piq_root_dir = sys.argv[3]
+    # bam_file_location = sys.argv[4]
+
+    # result = subprocess.call(['mkdir', piq_root_dir + 'matches'])
+    # if result == 0:
+    #     sys.exit("Error in creating matches directory for PIQ")
+    # result = subprocess.call(['mkdir', piq_root_dir + 'matches/motif.matches'])
+    # if result == 0:
+    #     sys.exit("Error in creating motif matches directory for PIQ")
+    # result = subprocess.call(['rscript', 'pwmmatch.exact.r', piq_root_dir + '/common.r', \
+    #     piq_root_dir + './pwms/jasparfix.txt', '139', piq_root_dir + './matches/motif.matches/'])
+    # if result == 0:
+    #     sys.exit("Error in step 3")
+    # result = subprocess.call(['mkdir', piq_root_dir + '/RData'])
+    # if result == 0:
+    #     sys.exit("Error in step 4")
+    # result = subprocess.call(['rscript', piq_root_dir + 'bam2rdata.r', piq_root_dir + '/common.r', \
+    #     piq_root_dir + '/RData/d0.RData', bam_file_location])
+    # if result == 0:
+    #     sys.exit("Error in step 5")
+    # result = subprocess.call(['mkdir', piq_root_dir + '/output'])
+    # if result == 0:
+    #     sys.exit("Error in step 6")
+    # result = subprocess.call('mkdir', piq_root_dir + '/tmp')
+    # if result == 0:
+    #     sys.exit("Error in step 7")
+    # result = subprocess.call(['rscript', piq_root_dir + 'pertf.r', piq_root_dir + '/common.r', \
+    #     piq_root_dir + '/matches/motif.matches', piq_root_dir + '/tmp', \
+    #     piq_root_dir + '/output', piq_root_dir + '/RData/d0.RData', '139'])
+    # if result == 0:
+    #     sys.exit("Error in step 8")
+
+    # result = subprocess.call(['bash', piq_root_dir + '/PIQScripts.sh'])
+    # if result == 0:
+    #     sys.exit("Error in executing PIQ")
+
     create_piq_bed_file(input_csv_file, output_bed_file)
 
-    with open('dummy_file','r+') as f_handle:
+    # python PIQPredict.py /Users/AakashRavi/Desktop/Stanford/Education/Bioinformatics/piq-single/output/130130.mm10.d0/139-MA01391CTCF-calls.csv ../../output_file /Users/AakashRavi/Desktop/Stanford/Education/Bioinformatics/piq-single /Users/AakashRavi/Desktop/Stanford/Education/Bioinformatics/bams/ENCFF001CTZ.bam
+    # python PIQPredict.py /Users/AakashRavi/Desktop/Stanford/Education/Bioinformatics/piq-single/output/130130.mm10.d0/139-MA01391CTCF-calls.csv ../../output_file /Users/AakashRavi/Desktop/Stanford/Education/Bioinformatics/piq-single
 
-        peaks_and_labels = load_labeled_peaks_from_beds(
-            output_bed_file, f_handle)
+    # with open('dummy_file','r+') as f_handle:
 
-        intervals = get_intervals_from_peaks(peaks_and_labels)
-        bedtool = BedTool(intervals)
-        merged_bedtool = bedtool.sort().merge()
+    #     peaks_and_labels = load_labeled_peaks_from_beds(
+    #         output_bed_file, f_handle)
+
+    #     intervals = get_intervals_from_peaks(peaks_and_labels)
+    #     bedtool = BedTool(intervals)
+    #     merged_bedtool = bedtool.sort().merge()
 
 if __name__ == '__main__':
     main()
