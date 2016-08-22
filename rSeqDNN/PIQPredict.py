a97ab09d534b4d0d464c0cb59f1e71470f082021
Aakash Ravi <aakash_ravi@hotmail.com>
2016-08-22 13:43:56 +0200

    Resolving delimeter problem

--- rSeqDNN/PIQPredict.py
+++ rSeqDNN/PIQPredict.py
@@ -20,6 +20,7 @@ from pyTFbindtools.peaks import label_and_score_peak_with_chipseq_peaks
 from pyTFbindtools.peaks import load_chromatin_accessible_peaks_and_chipseq_labels_from_DB
 
 from pyTFbindtools.cross_validation import ClassificationResult
+import numpy as np
 
 # from pybedtools import Interval, BedTool
 
